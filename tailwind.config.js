2c139ae06ba7e24c90f4341ec4b778f19e44e272
Jamie151515 <yutinggggg0215@gmail.com>
2023-01-13 12:54:50 +0800

    新增網頁

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#3C78E4',
+        'accent': '#EA9388',
+        'lightblue': '#E7ECF2',
+        'fogblue': '#D3DAE0'
+      },
+    },
+  },
+  plugins: [],
+}
