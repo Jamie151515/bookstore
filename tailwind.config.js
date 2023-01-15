a22360a70f170cad560cddcf4edeb3fc9872b15b
Jamie151515 <yutinggggg0215@gmail.com>
2023-01-15 21:03:11 +0800

    新增圖片

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
         'primary': '#3C78E4',
         'accent': '#EA9388',
         'lightblue': '#E7ECF2',
-        'fogblue': '#D3DAE0'
+        'fogblue': '#D3DAE0',
+        'gray': '#6f6f6f',
       },
 
       fontFamily: {
         'helvetica-neue': ['"Helvetica Neue"'],
+        'inter': ['"Inter"'],
         }
 
     },
